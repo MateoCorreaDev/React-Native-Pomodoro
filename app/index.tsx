cf87d1a3dd2990150732190d66a0809ed7adab03
MateoCorreaDev <mateo.dev738@gmail.com>
2024-06-17 19:14:44 -0500

    Cleaning Default project

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,32 @@
-import { Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet,Text, View } from "react-native";
+import { useState } from "react";
+
+const colors = ["#F7DC6F","#A2D9C3","D7BDE2"]
 
 export default function Index() {
+  const [isWorking, setIsWorking] = useState(false);
+  const [time, setTime] = useState(25*60);
+  const [currentTime, setCurrentTime] = useState("POMO" | "SHORT" | "BREAK");
+ 
+ 
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text style = {styles.text}>Pomodoro</Text>
+      <Text style = {styles.text}>{time}</Text>
+      <StatusBar style= "dark"/>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingLeft: 10,
+  },
+  text: {
+    fontSize: 32,
+    fontWeight: "900", 
+  }
+})
